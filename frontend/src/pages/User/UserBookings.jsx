bc13d2e8d66ff95f3a2cd7de647096ee226abf4c
Naleem Mohamed Nabris <naleemmohamednabris@Naleems-MacBook-Air.local>
2026-04-25 00:09:56 +0530

    Implement booking user booking

--- frontend/src/pages/User/UserBookings.jsx
+++ frontend/src/pages/User/UserBookings.jsx
@@ -1,3 +1,5 @@
+
+
 import React, { useState, useEffect } from 'react';
 import { getMyBookings, cancelBooking } from '../../services/bookingApi';
 import { getResources } from '../../services/catalogueApi';
@@ -257,3 +259,4 @@ const UserBookings = () => {
 };
 
 export default UserBookings;
+
