26f57f68ee028a96520c94fd65ec9d6ab05bd213
Naleem Mohamed Nabris <naleemmohamednabris@Naleems-MacBook-Air.local>
2026-04-24 17:53:30 +0530

    Implement update booking API

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 import { Search, Sparkles } from 'lucide-react';
 import Navbar from '../components/Navbar';
@@ -54,3 +56,4 @@ const MainLayout = ({ children }) => {
 };
 
 export default MainLayout;
+
